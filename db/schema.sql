5d47b76de17e1827fbd5ba13fde0b52fcd540596
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-05-14 10:17:14 -0400

    code working but needs styling and readme

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATEBASE burger_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers
+(
+    id INTEGER
+    auto_increment not null,
+    burger_name VARCHAR
+    (50),
+    devoured BOOLEAN DEFAULT false,
+    primary key
+    (id)
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+values('Beef Burger', false),
+    ('chicken-Burger', false),
+    ('impossible Burger', false);
+
+select *
+from burgers;
